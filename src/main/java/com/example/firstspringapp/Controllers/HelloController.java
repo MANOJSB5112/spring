66b8928a1852490fd13ac0bbf79a7f07c95eeb21
MANOJ S B <manojsb5112@gmail.com>
2024-06-12 17:16:27 +0530

    updated hellocontroller

--- src/main/java/com/example/firstspringapp/Controllers/HelloController.java
+++ src/main/java/com/example/firstspringapp/Controllers/HelloController.java
@@ -1,15 +1,16 @@
 package com.example.firstspringapp.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/hello")
 public class HelloController {
-    @GetMapping("/print")
-    public String printHello(String name)
+    @GetMapping("/print/{firstname}/{secondname}")
+    public String printHello(@PathVariable String firstname,@PathVariable String secondname)
     {
-        return "Hello There my name is manoj";
+        return "Hello There my name is "+firstname+" "+ secondname;
     }
 }
